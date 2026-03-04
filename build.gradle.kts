e3da0f39366d1b1c3e88d5fb21a36d1cd6f679be
naburnm8 <rtender.artem@gmail.com>
2026-03-04 18:10:36 +0300

    add maven publishing configuration for adaptiveQueue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.10"
+    `maven-publish`
 }
 
 group = "ru.bmstu.naburnm8"
@@ -18,4 +19,16 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "ru.bmstu.naburnm8"
+            artifactId = "adaptiveQueue"
+            version = "1.0-SNAPSHOT"
+
+            from(components["kotlin"])
+        }
+    }
 }
